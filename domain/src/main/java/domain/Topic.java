e9e19dcf5c310642cabb09b4304f7efce2b9b1a8
Davit Karchikyan <davit.karchikyan@iunetworks.am>
2019-03-06 15:44:25 +0400

    fix yml configuration file

---
+++ domain/src/main/java/domain/Topic.java
@@ -0,0 +1,19 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic {
+
+    private String mainImagePath;
+    private String imagesPaths;
+    private String descriptions;
+    private Double price;
+    private Integer AverageRating;
+}
